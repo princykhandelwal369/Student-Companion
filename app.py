4180cf6a42bdd31bd31da470dcf89d2e1aa8ddcf
princykhandelwal369 <princykhandelwal369@gmail.com>
2026-07-05 21:12:52 +0530

    Jinja template

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", name="Princy", year=2, course="B.Tech", cgpa="8.5")
 
 @app.route("/about")
 def about():
